b17d14772308c6e9b2bd2ef001ad926d3acc4fa0
ceren <ceren@DESKTOP-SDSFKH7>
2022-09-16 22:01:01 +0300

    first commit

---
+++ cerenp/src/cerenp/Main.java
@@ -0,0 +1,10 @@
+package cerenp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ cerenp/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ceren
+ *
+ */
+module cerenp {
+}
